f3972e7b438d09e6eca8c0e5687eefbde37122b5
Denis Singh <denis.darkrgd@gmail.com>
2021-11-14 18:07:15 +0200

    Added base files

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { lazy, Suspense } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Container from 'components/Container';
+const HomeView = lazy(() => import('./views/HomeView'));
+const HeroView = lazy(() => import('./views/HeroView'));
+
+export default function App() {
+  return (
+    <Container>
+      <Switch>
+        <Suspense fallback={''}>
+          <Route exact path="/">
+            <HomeView />
+          </Route>
+
+          <Route path="/heroes/:id">
+            <HeroView />
+          </Route>
+        </Suspense>
+      </Switch>
+    </Container>
+  );
+}
