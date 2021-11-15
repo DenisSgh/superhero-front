a3653f90588fdbc0c79c9adf3b36110c9f16c626
Denis Singh <denis.darkrgd@gmail.com>
2021-11-15 17:37:56 +0200

    Update views

--- src/views/HeroView.js
+++ src/views/HeroView.js
@@ -1,15 +1,10 @@
-// import Filter from 'components/Filter';
-// import Contacts from 'components/Contacts';
+import HeroDetails from 'components/HeroDetails';
 import s from './Views.module.css';
-// import ModalEdit from 'components/ModalEdit/ModalEdit';
 
 export default function ContactsView() {
   return (
     <div className={s.contactsContainer}>
-      <h2 className={s.mainTitle}>PHONEBOOK</h2>
-      {/* <ModalEdit action="edit" />
-      <Filter />
-      <Contacts /> */}
+      <HeroDetails />
     </div>
   );
 }
--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,31 +1,13 @@
-import { useEffect, useState } from 'react';
-// import { Link } from 'react-router-dom';
 import s from './Views.module.css';
 import ModalAdd from 'components/ModalAdd';
 import Heroes from 'components/Heroes';
-import { fetchHeroes } from 'components/ApiService';
 
 export default function HomePage() {
-  const [heroes, setHeroes] = useState([]);
-  const [totalDocs, setTotalDocs] = useState(0);
-  const [page, setPage] = useState(1);
-
-  useEffect(() => {
-    fetchHeroes(page).then(res => setHeroes(res.heroes.docs));
-    setTotalDocs(heroes.totalDocs);
-    // dispatch(fetchContacts());
-  }, []);
-
-  useEffect(() => {
-    setTotalDocs(heroes.totalDocs);
-    // dispatch(fetchContacts());
-  }, [heroes, totalDocs]);
-
   return (
     <>
       <h1 className={s.title}>Welcome!</h1>
       <ModalAdd action="add" />
-      <Heroes setPage={setPage} heroes={heroes} totalDocs={totalDocs} />
+      <Heroes />
     </>
   );
 }
